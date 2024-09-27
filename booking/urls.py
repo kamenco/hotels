efa1e5e053347b70d6103a0cd6f9a3fb1dc17c99
Kamen Kirov <kamen@bgtown.com>
2024-09-27 17:22:42 +0000

    Updated urls.py for favicon to be ignored.

--- booking/urls.py
+++ booking/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,12 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+def empty_favicon(request):
+    return HttpResponse(status=204)
+
+urlpatterns = [
+    # Your other paths here...
+    path('favicon.ico', empty_favicon),
+]
