4e8c46d4e9ab34da14af3a580ea7d08d15c9ba8d
Kamen Kirov <kamen@bgtown.com>
2024-09-28 20:21:44 +0000

    Secret key deleted.

--- booking/settings.py
+++ booking/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kamenco-hotels-wdkmicfp65i.ws.codeinstitute-ide.net', 'localhost',]
 
